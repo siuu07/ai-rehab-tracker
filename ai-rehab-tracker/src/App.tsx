cfc24cbd8786dfd826f88467ec184bfd2e2d1a04
rondo09 <rondo09@students.cs.ubc.ca>
2026-01-27 19:38:27 +0530

    day click feature

--- ai-rehab-tracker/src/App.tsx
+++ ai-rehab-tracker/src/App.tsx
@@ -4,18 +4,24 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  const [surgeryDate, setSurgeryDate] = useState("");
   const [pain, setPain] = useState("");
   const [pains, setPains] = useState<{ pain: number, date: string }[]>([]);
   const today = new Date();
   const year = today.getFullYear();
   const month = today.getMonth();
   const daysInMonth = new Date(year, month + 1, 0).getDate();
+  const [selectedDay, setSelectedDay] = useState<number | null>(null);
 
   function getColor(pain: number) {
     if (pain <= 3) return "green";
     if (pain <= 6) return "orange";
     return "red";
   }
+  // function daysFromSurgery(date: Date) {
+  //   const diff = date.getTime() - surgeryDate.getTime();
+  //   return Math.floor(diff / (1000 * 60 * 60 * 24));
+  // }
   return (
     <>
       {/* <div>
@@ -23,6 +29,14 @@ function App() {
       <h1>AI-assisted Rehab Tracker</h1>
       <p>Inspired by ACL recovery!</p>
       <div className="card">
+        <label>Enter your surgery date:
+          <input
+            type="date"
+            value={surgeryDate}
+            onChange={(e) => setSurgeryDate(e.target.value)}
+          />
+        </label>
+        <br />
         <label>Enter your pain:
           <input
             type="number"
@@ -59,21 +73,22 @@ function App() {
           const entry = pains.find(p => new Date(p.date).getDate() === day);
           const color = entry ? getColor(entry.pain) : "#eee";
           return (
-            <div
-              className="day"
+            <div className="day"
               key={day}
+              onClick={() => setSelectedDay(day)}
               style={{
                 backgroundColor: color,
-                border: isToday ? "3px solid #3b82f6" : "1px solid #ccc",
+                //border: isToday ? "3px solid #3b82f6" : "1px solid #ccc",
                 fontWeight: isToday ? "bold" : "normal",
                 transform: isToday ? "scale(1.05)" : "scale(1)",
+                border: selectedDay === day ? "3px solid black" : "1px solid #ccc",
               }}
             >
-        {day}
-      </div>
-      );
+              {day}
+            </div>
+          );
         })}
-    </div >
+      </div >
 
     </>
   )
